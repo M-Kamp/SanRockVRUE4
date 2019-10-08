74466cf54cb1a2bd83ab0c5b3eb8f528e864b99e
Laursen44 <mikkel_laursen88@hotmail.com>
2019-10-08 09:53:28 +0200

    Init

---
+++ SanRockVR/Intermediate/Build/Android/UE4/Development/SanRockVRMED7/HelloWorld.gen.cppa7.d
@@ -0,0 +1,8 @@
+C:/Users/Mikke/Documents/Unreal\ Projects/SanRockVR-repo/SanRockVR/Intermediate/Build/Android/UE4/Development/SanRockVRMED7/HelloWorld.gen.cppa7.o: \
+  C:/Users/Mikke/Documents/Unreal\ Projects/SanRockVR-repo/SanRockVR/Intermediate/Build/Android/UE4/Inc/SanRockVRMED7/HelloWorld.gen.cpp \
+  C:/Users/Mikke/Documents/Unreal\ Projects/SanRockVR-repo/SanRockVR/Intermediate/Build/Android/SanRockVRMED7/Development/Engine/SharedPCH.Engine.h \
+  C:/Users/Mikke/Documents/Unreal\ Projects/SanRockVR-repo/SanRockVR/Intermediate/Build/Android/UE4/Development/SanRockVRMED7/Definitions.SanRockVRMED7.h \
+  C:/Program\ Files/Epic\ Games/UE_4.23/Engine/Source/Runtime/CoreUObject/Public\UObject/GeneratedCppIncludes.h \
+  C:/Program\ Files/Epic\ Games/UE_4.23/Engine/Source/Runtime/CoreUObject/Public\UObject/MetaData.h \
+  C:/Users/Mikke/Documents/Unreal\ Projects/SanRockVR-repo/SanRockVR/Source\SanRockVRMED7/Public/HelloWorld.h \
+  C:/Users/Mikke/Documents/Unreal\ Projects/SanRockVR-repo/SanRockVR/Intermediate/Build/Android/UE4/Inc/SanRockVRMED7\HelloWorld.generated.h
---
+++ SanRockVR/Intermediate/Build/Win64/UE4Editor/Inc/SanRockVRMED7/HelloWorld.generated.h
@@ -0,0 +1,90 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef SANROCKVRMED7_HelloWorld_generated_h
+#error "HelloWorld.generated.h already included, missing '#pragma once' in HelloWorld.h"
+#endif
+#define SANROCKVRMED7_HelloWorld_generated_h
+
+#define Test_4_23_Source_SanRockVRMED7_Public_HelloWorld_h_13_RPC_WRAPPERS
+#define Test_4_23_Source_SanRockVRMED7_Public_HelloWorld_h_13_RPC_WRAPPERS_NO_PURE_DECLS
+#define Test_4_23_Source_SanRockVRMED7_Public_HelloWorld_h_13_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUHelloWorld(); \
+	friend struct Z_Construct_UClass_UHelloWorld_Statics; \
+public: \
+	DECLARE_CLASS(UHelloWorld, UActorComponent, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/SanRockVRMED7"), NO_API) \
+	DECLARE_SERIALIZER(UHelloWorld)
+
+
+#define Test_4_23_Source_SanRockVRMED7_Public_HelloWorld_h_13_INCLASS \
+private: \
+	static void StaticRegisterNativesUHelloWorld(); \
+	friend struct Z_Construct_UClass_UHelloWorld_Statics; \
+public: \
+	DECLARE_CLASS(UHelloWorld, UActorComponent, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/SanRockVRMED7"), NO_API) \
+	DECLARE_SERIALIZER(UHelloWorld)
+
+
+#define Test_4_23_Source_SanRockVRMED7_Public_HelloWorld_h_13_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UHelloWorld(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UHelloWorld) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UHelloWorld); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UHelloWorld); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UHelloWorld(UHelloWorld&&); \
+	NO_API UHelloWorld(const UHelloWorld&); \
+public:
+
+
+#define Test_4_23_Source_SanRockVRMED7_Public_HelloWorld_h_13_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UHelloWorld(UHelloWorld&&); \
+	NO_API UHelloWorld(const UHelloWorld&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UHelloWorld); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UHelloWorld); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(UHelloWorld)
+
+
+#define Test_4_23_Source_SanRockVRMED7_Public_HelloWorld_h_13_PRIVATE_PROPERTY_OFFSET
+#define Test_4_23_Source_SanRockVRMED7_Public_HelloWorld_h_10_PROLOG
+#define Test_4_23_Source_SanRockVRMED7_Public_HelloWorld_h_13_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Test_4_23_Source_SanRockVRMED7_Public_HelloWorld_h_13_PRIVATE_PROPERTY_OFFSET \
+	Test_4_23_Source_SanRockVRMED7_Public_HelloWorld_h_13_RPC_WRAPPERS \
+	Test_4_23_Source_SanRockVRMED7_Public_HelloWorld_h_13_INCLASS \
+	Test_4_23_Source_SanRockVRMED7_Public_HelloWorld_h_13_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Test_4_23_Source_SanRockVRMED7_Public_HelloWorld_h_13_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Test_4_23_Source_SanRockVRMED7_Public_HelloWorld_h_13_PRIVATE_PROPERTY_OFFSET \
+	Test_4_23_Source_SanRockVRMED7_Public_HelloWorld_h_13_RPC_WRAPPERS_NO_PURE_DECLS \
+	Test_4_23_Source_SanRockVRMED7_Public_HelloWorld_h_13_INCLASS_NO_PURE_DECLS \
+	Test_4_23_Source_SanRockVRMED7_Public_HelloWorld_h_13_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> SANROCKVRMED7_API UClass* StaticClass<class UHelloWorld>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Test_4_23_Source_SanRockVRMED7_Public_HelloWorld_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
